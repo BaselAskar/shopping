8d147b6fbec8b53a1047d0fb4604d4d50c591815
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-11-24 14:16:53 +0100

    Initial Models

---
+++ src/main/java/org/codewithbasel/Model/Product.java
@@ -0,0 +1,41 @@
+package org.codewithbasel.Model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
